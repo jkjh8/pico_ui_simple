53c2380a9bc7df99b33b8197dd9e909bdab47f0d
jkjh8 <jkjh82@naver.com>
2025-10-21 18:01:13 +0900

    bootstrap.js 파일을 추가하고 purgecss 플러그인을 제거하여 빌드 성능을 개선함

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './style.css'
 import router from './router'
 import { Collapse } from 'bootstrap'

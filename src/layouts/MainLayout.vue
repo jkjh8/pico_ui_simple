0c593debc3a403e6678536dac852c4c273f0b300
jkjh8 <jkjh82@naver.com>
2025-10-22 10:01:30 +0900

    네비게이션 바의 구조를 간소화하고 라우터 링크로 변경하여 코드 가독성을 향상시킴. 웹 해시 히스토리로 라우터 설정을 수정함.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,14 +2,12 @@
 
 <script setup>
   import { useRoute } from 'vue-router'
-  import { ref } from 'vue'
   const route = useRoute()
-  const navbarCollapse = ref(null)
 </script>
 
 <template>
   <nav
-    class="navbar navbar-expand-md px-4 bg-primary"
+    class="navbar navbar-expand px-4 bg-primary"
     data-bs-theme="dark">
     <div class="container-fluid">
       <div
@@ -17,40 +15,27 @@
         style="font-weight: 600">
         Mic Controller
       </div>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div
-        class="collapse navbar-collapse"
-        id="navbarNavAltMarkup"
-        ref="navbarCollapse">
-        <div class="navbar-nav">
-          <a
+      <div class="d-flex align-items-center w-100">
+        <div class="navbar-nav d-flex flex-row flex-nowrap">
+          <router-link
             class="nav-link"
             :class="{ active: route.path === '/' }"
-            href="/"
+            to="/"
             aria-current="page">
             Settings
-          </a>
-          <a
+          </router-link>
+          <router-link
             class="nav-link"
             :class="{ active: route.path === '/comm' }"
-            href="/comm">
+            to="/comm">
             Communication
-          </a>
-          <a
+          </router-link>
+          <router-link
             class="nav-link"
             :class="{ active: route.path === '/gpio' }"
-            href="/gpio">
+            to="/gpio">
             GPIO
-          </a>
+          </router-link>
         </div>
         <span class="navbar-text ms-auto">v0.1.0</span>
       </div>
